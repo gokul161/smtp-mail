a48e956c82bac49ec64940076e9c4e077855c912
gokul161 <94886848+gokul161@users.noreply.github.com>
2022-04-16 09:06:14 +0530

    Add files via upload

---
+++ Example.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include "Mail.h"
+int main()
+{
+
+	//AFter Second run clear everything from main method and use SendMail(0);
+	SendMail(0);
+}
